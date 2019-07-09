1ee724940e85e182c26960b0b6e721aabd42835e
Drew Seeholzer <drews21f7@gmail.com>
2019-07-09 16:16:42 -0600

    Made post and comment model files

---
+++ Continuum/Models/Comment.swift
@@ -0,0 +1,20 @@
+//
+//  Comment.swift
+//  Continuum
+//
+//  Created by Drew Seeholzer on 7/9/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import Foundation
+
+class Comment {
+    var text: String
+    var timestamp: Date
+    weak var post: Post?
+    init(text: String, timestamp: Date = Date(), post: Post) {
+        self.text = text
+        self.timestamp = timestamp
+        self.post = post
+    }
+}
