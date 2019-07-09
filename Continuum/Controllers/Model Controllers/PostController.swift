4ec8e1239f1de82a382f1237f62035b2d162f5a3
Drew Seeholzer <drews21f7@gmail.com>
2019-07-09 16:29:34 -0600

    Setup model controller singleton, SOT, and addComment and createPostWith functions.

---
+++ Continuum/Controllers/Model Controllers/PostController.swift
@@ -0,0 +1,26 @@
+//
+//  PostController.swift
+//  Continuum
+//
+//  Created by Drew Seeholzer on 7/9/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import UIKit
+
+class PostController {
+    
+    let sharedInstance = PostController()
+    
+    var posts: [Post] = []
+    
+    func addComment(text: String, post: Post, completion: (Comment) -> Void) {
+        let comment = Comment(text: text, post: post)
+        post.comments.append(comment)
+    }
+    
+    func createPostWith(photo: UIImage, caption: String, completion: (Post?) -> Void) {
+        let post = Post(photo: photo, caption: caption)
+        posts.append(post)
+    }
+}
